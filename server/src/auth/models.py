127c7058daea0f5e098957a5f0bcf704b6db86d9
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-07-20 11:17:07 +0200

    feat: add UserType model and link it to User

--- server/src/auth/models.py
+++ server/src/auth/models.py
@@ -1,5 +1,11 @@
 from sqlmodel import Field, SQLModel
 
+class UserType(SQLModel, table=True):
+    __tablename__ = "users_type"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, index=True)
+
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
@@ -7,3 +13,5 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, index=True, nullable=False)
     email: str = Field(unique=True, index=True, nullable=False)
     password_hash: str = Field(nullable=False)
+
+    user_type_id: int | None = Field(default=None, foreign_key="users_type.id")
