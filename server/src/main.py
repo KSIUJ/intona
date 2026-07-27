1f2eec5b3af8b75781de1fdbe10215dab9f4dcf4
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-07-27 17:41:39 +0200

    chore: add router to main.py

--- server/src/main.py
+++ server/src/main.py
@@ -12,6 +12,7 @@ from src.auth.router import router as auth_router
 from src.exercises.router import router as exercise_router
 from src.stats.router import router as stats_router
 from src.vocal_analysis.router import router as vocal_analysis_router
+from src.logs.router import router as logs_router
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
@@ -35,6 +36,7 @@ app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
 app.include_router(exercise_router, prefix="/api/exercises", tags=["exercises"])
 app.include_router(stats_router, prefix="/api/users", tags=["users"])
 app.include_router(vocal_analysis_router, prefix="/api/vocal_analysis", tags=["vocal_analysis"])
+app.include_router(logs_router, prefix="/api/logs", tags=["logs"])
 
 logger = logging.getLogger("uvicorn.error")
 
