cb75da2198de7564bf68f4a47ffca72f91210790
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-07-27 22:04:42 +0200

    chore(models): add datetime.now default factory to attempted_at

--- server/src/logs/models.py
+++ server/src/logs/models.py
@@ -11,7 +11,10 @@ class ExerciseLogs(SQLModel, table=True):
     exercise_duration: int = Field(nullable=False)
     time_in_tune: float = Field(nullable=False)
     average_deviation: float = Field(nullable=False)
-    attempted_at: datetime = Field(nullable=False)
+    attempted_at: datetime = Field(
+        default_factory=datetime.now,
+        nullable=False
+    )
     attempting_user_id: int = Field(nullable=False, foreign_key="users.id")
 
     user: "User" = Relationship(back_populates="logs")
