d1d8e05da571f092a4828616bf60818c5eb2c101
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-07-28 10:42:52 +0200

    refactor: replace anext(get_db) with SessionDep dependency

--- server/src/stats/dependencies.py
+++ server/src/stats/dependencies.py
@@ -4,13 +4,12 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel import select
 
-from src.database import get_db
+from src.database import get_db, SessionDep
 from src.stats.models import UserStats
 
 
-async def check_user_stats(user_id: int) -> UserStats:
-    db = await anext(get_db())
-    user_stats: UserStats = await db.exec(select(UserStats).where(UserStats.id == user_id))
+async def check_user_stats(user_id: int, session: SessionDep) -> UserStats:
+    user_stats: UserStats = await session.exec(select(UserStats).where(UserStats.id == user_id))
     user_stats = user_stats.first()
     # maybe i should commit this change to a database
     if (datetime.now().date() - user_stats.lastActivityDate.date()).days > 1:
