d53d0672dda08d1e15a0ee7f5ef82a33b7a326d2
Rac00nCanFly <aleksandra.dziurok@gmail.com>
2026-08-04 13:06:03 +0200

    Basic routing plus minimal layout home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
 
-function App() {
+function Login(){
   function handleSubmit(event) {
     event.preventDefault();
   }
-
   return (
   <div className="app">
     <header className="site-header">
@@ -14,9 +14,9 @@ function App() {
       </a>
 
       <nav className="main-nav">
-        <a href="#home">Home</a>
-        <a href="#about">About us</a>
-        <a href="#contact">Contact</a>
+        <Link to="/home"> Home</Link>
+        <Link to="/about">About us</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
 
       <div className="auth-nav">
@@ -83,6 +83,90 @@ function App() {
     </main>
   </div>
 );
+
+}
+
+function Home() {
+  const navItems = ["Home", "Dashboard"];
+
+  const exercises = [
+    "G-major scale",
+    "Intervals 2-3",
+    "Intervals 4-5",
+    "Intervals 6-7",
+    "Intervals 1-8",
+    "C-major scale",
+    "F-major scale",
+  ]
+  const songs = [
+    "Song 1",
+    "Song 2",
+    "Song 3",
+    "Song 4",
+  ]
+  return (
+  <div className="min-h-screen">
+   <nav >
+      <ul className="mx-auto flex max-w-6xl items-center gap-12 h-18">
+        {navItems.map((item) => (
+          <li key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+   </nav>
+   <main>
+    <h1>Welcome to Intona!</h1>
+    <div className="grid grid-cols-2 gap-8">
+    <section>
+      <h2>Exercises</h2>
+
+      <ul>
+        {exercises.map((exercise) => (
+          <li key={exercise}>
+            {exercise}
+          </li>
+        ))}
+      </ul>
+    </section>
+    <section>
+    <h2>Songs</h2>
+        <ul>{songs.map((song) => (
+          <li key={song}>
+            {song}
+          </li>
+))}
+        </ul>
+</section>
+</div>
+   </main>
+  </div>
+  
+  );
+}
+
+function About() {
+  return (
+    <h1>About</h1>
+  );
+}
+
+function Contact(){
+  return (
+    <h1>Contact</h1>
+  );
+}
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
