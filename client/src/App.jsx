303716af01348ca1c7bee86b5512aee9690d11a2
Rac00nCanFly <aleksandra.dziurok@gmail.com>
2026-08-05 16:03:45 +0200

    Preliminary design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { DataGrid } from '@mui/x-data-grid';
+import { ExerciseTable } from "./Selection";
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
 
 function Login(){
   function handleSubmit(event) {
@@ -86,23 +91,49 @@ function Login(){
 
 }
 
+
+function ExercisePage() {
+  const { exerciseSlug } = useParams();
+
+  return (
+    <main>
+      <h1>Exercise page</h1>
+      <p>Selected exercise: {exerciseSlug}</p>
+    </main>
+  );
+}
+function SongsPage() {
+  const { songsSlug } = useParams();
+
+  return (
+    <main>
+      <h1>Songs page</h1>
+      <p>Selected song: {songsSlug}</p>
+    </main>
+  );
+}
+
 function Home() {
- 
-  const exercises = [
-    "G-major scale",
-    "Intervals 2-3",
-    "Intervals 4-5",
-    "Intervals 6-7",
-    "Intervals 1-8",
-    "C-major scale",
-    "F-major scale",
-  ]
-  const songs = [
-    "Song 1",
-    "Song 2",
-    "Song 3",
-    "Song 4",
-  ]
+  
+ const exercises = [
+  { id:1, title: "G-major scale", slug: "g-major-scale", difficulty: "Easy", rating: "100%" },
+  { id:2, title: "C-major scale", slug: "c-major-scale" , difficulty: "Easy", rating: "100%"},
+  { id:3, title: "F-major scale", slug: "f-major-scale" , difficulty: "Easy", rating: "100%"},
+  { id:4, title: "Intervals 2-3", slug: "intervals-2-3" , difficulty: "Medium", rating: "90%"},
+  { id:5, title: "Intervals 4-5", slug: "intervals-4-5" , difficulty: "Medium", rating: "95%"},
+  { id:6, title: "Intervals 6-7", slug: "intervals-6-7" , difficulty: "Medium", rating: "97%"},
+  { id:7, title: "Intervals 1-8", slug: "intervals-1-8" , difficulty: "Hard", rating: "50%"},
+];
+const songs = [
+  { id: 1, title: "Song 1", slug: "song-1", difficulty: "Easy", rating: "100%" },
+  { id: 2, title: "Song 2", slug: "song-2", difficulty: "Easy", rating: "100%" },
+  { id: 3, title: "Song 3", slug: "song-3", difficulty: "Easy", rating: "100%" },
+  { id: 4, title: "Song 4", slug: "song-4", difficulty: "Medium", rating: "90%" },
+  { id: 5, title: "Song 5", slug: "song-5", difficulty: "Medium", rating: "95%" },
+  { id: 6, title: "Song 6", slug: "song-6", difficulty: "Medium", rating: "97%" },
+  { id: 7, title: "Song 7", slug: "song-7", difficulty: "Hard", rating: "50%" },
+];
+  
   return (
   <div className="app">
     <header className="site-header">
@@ -116,38 +147,23 @@ function Home() {
         <Link to="/exercises"> Exercises</Link>
         <Link to="/songs"> Songs</Link>
       </nav>
+      <Avatar className = "avatar" alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
 
-      <div className="auth-nav">
-        <button className="login-link" type="button">
-          <Link to="/"> Log in</Link>
-        </button>
-
-        <button className="signup-button" type="button">
-          Sign up
-        </button>
-      </div>
     </header>
    <main className="home-page">
-    <h1 className="home-header">Welcome to Intona!</h1>
+    <div className="home-header">
+    <h1 className="signup-button">INTONA</h1>
+    <h1 className="home-header">learn to be in tune!</h1>
+    </div>
     <div className="selection-boxes">
     <section className="selection-box">
-      <h2 className="selection-title"><Link to="/exercises"> Exercises</Link></h2>
-      <ul>
-        {exercises.map((exercise) => (
-          <li key={exercise}>
-            {exercise}
-          </li>
-        ))}
-      </ul>
+      <h2 className="selection-title"><Link to="/exercises">Exercises</Link></h2>
+      <h3><ExerciseTable isSong={false} /></h3>
     </section>
     <section className="selection-box">
     <h2 className="selection-title"><Link to="/songs"> Songs</Link></h2>
-        <ul>{songs.map((song) => (
-          <li key={song}>
-            {song}
-          </li>
-))}
-        </ul>
+    <h3><ExerciseTable isSong={true} /></h3>
+
 </section>
 </div>
    </main>
@@ -174,11 +190,7 @@ function Dashboard(){
   );
 }
 
-function Exercises(){
-  return (
-    <h1>Exercises</h1>
-  );
-}
+
 
 function Songs(){
   return (
@@ -194,8 +206,11 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/exercises" element={<Exercises />} />
+      <Route path="/exercises" element={<ExercisePage />} />
+      <Route path="/exercises/:exerciseSlug" element={<ExercisePage />} />
       <Route path="/songs" element={<Songs />} />
+      <Route path="/songs/:songsSlug" element={<SongsPage />} />
+
     </Routes>
     </BrowserRouter>
   );
