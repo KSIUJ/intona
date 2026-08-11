a98fe20e626ce80921b5ba2d9f55057d3422f598
DominikWojtanowski <dominik.wojtanowski@interia.pl>
2026-08-11 04:30:01 +0200

    chore(config): add vercel configuration for hosting frontend and backend in the same domain

--- client/README.txt
+++
@@ -1,2 +0,0 @@
-Link to web page: https://intona-two.vercel.app/
-
---
+++ client/frontend/src/components/ExercisesList.jsx
@@ -0,0 +1,38 @@
+import {Link, Route, Routes} from 'react-router-dom'
+import {useQuery} from '@tanstack/react-query';
+import OngoingExercise from './OngoingExercise.jsx'
+
+
+const fetchExercises = async () => {
+    const response = await fetch(`/api/exercises`, {
+        credentials: 'include',
+    });
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+    return await response.json();
+}
+
+export default function ExercisesList() {
+    const { data, error, isLoading, isError } = useQuery({ queryKey: ["exercises"], queryFn: fetchExercises })
+
+    if (isLoading) return <p>Ładowanie ćwiczeń...</p>;
+    if (isError) return <p>Błąd: {error.message}</p>;
+
+    return (
+        <Routes>
+            <Route index
+                element={
+                    <nav>
+                        {data.map((exercise) => (
+                            <Link key={exercise.id} to={`exercises/${exercise.id}`}>
+                                Exercise {exercise.id}
+                            </Link>
+                        ))}
+                    </nav>
+                } />
+
+            <Route path="exercises/:id" element={<OngoingExercise />} />
+        </Routes>
+    )
+}
