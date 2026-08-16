52d36e6899c29853fdd0429ad0dd645b9576d5ce
DominikWojtanowski <dominik.wojtanowski@interia.pl>
2026-08-16 07:48:00 +0200

    feat(actualizing): added actualizing favorite exercise, days active, total practice time, and changed slightly actualizing user stats

--- server/src/vocal_analysis/utils.py
+++ server/src/vocal_analysis/utils.py
@@ -1,5 +1,6 @@
 from sqlmodel import select, func
 from sqlalchemy.orm.attributes import flag_modified
+
 from src.exercises.models import Exercise
 from src.logs.models import ExerciseLogs
 from src.stats.models import UserStats
@@ -7,49 +8,49 @@ from src.stats.utils import actualize_user_streak
 from src.database import get_db
 
 
-async def add_exercise_result(user_id: int, exerciseLog: ExerciseLogs):
-    db = await anext(get_db())
+async def add_exercise_result(user_id: int, exercise_log: ExerciseLogs):
+    async for db in get_db():
+        detailed_user_stats: UserStats = await db.exec(select(UserStats).where(UserStats.id == int(user_id)))
+        detailed_user_stats = detailed_user_stats.first()
+        detailed_user_stats.average_score = (detailed_user_stats.average_score * detailed_user_stats.exercises_completed + exercise_log.time_in_tune) / (detailed_user_stats.exercises_completed + 1)
+        detailed_user_stats.exercises_completed += 1
 
-    detailed_user_stats: UserStats = await db.exec(select(UserStats).where(UserStats.id == int(user_id)))
-    detailed_user_stats = detailed_user_stats.first()
-    detailed_user_stats.averageScore = (detailed_user_stats.averageScore * detailed_user_stats.exercisesCompleted + exerciseLog.time_in_tune) / (detailed_user_stats.exercisesCompleted + 1)
-    detailed_user_stats.exercisesCompleted += 1
-
-    biggest_score = await db.exec(select(func.max(ExerciseLogs.time_in_tune)).where(ExerciseLogs.exercise_id == exerciseLog.exercise_id).where(ExerciseLogs.attempting_user_id == int(user_id)))
-    biggest_score = biggest_score.first()
+        biggest_score = await db.exec(select(func.max(ExerciseLogs.time_in_tune)).where(ExerciseLogs.exercise_id == exercise_log.exercise_id).where(ExerciseLogs.attempting_user_id == int(user_id)))
+        biggest_score = biggest_score.first()
 
 
-    if (exerciseLog.time_in_tune > 80 and (not biggest_score or biggest_score < 80)):
-        mastered_count = await db.exec(select(func.count(ExerciseLogs.exercise_id.distinct())).where(ExerciseLogs.attempting_user_id == int(user_id)).where(ExerciseLogs.time_in_tune >= 80))
-        mastered_count = mastered_count.first() + 1
-        exercises_count = await db.exec(select(func.count(Exercise.id)))
-        exercises_count = exercises_count.first()
-        if exercises_count > 0:
-            detailed_user_stats.masteredPercentage = mastered_count / exercises_count * 100
+        if (exercise_log.time_in_tune > 80 and (not biggest_score or biggest_score < 80)):
+            mastered_count = await db.exec(select(func.count(ExerciseLogs.exercise_id.distinct())).where(ExerciseLogs.attempting_user_id == int(user_id)).where(ExerciseLogs.time_in_tune >= 80))
+            mastered_count = mastered_count.first() + 1
+            exercises_count = await db.exec(select(func.count(Exercise.id)))
+            exercises_count = exercises_count.first()
+            if exercises_count > 0:
+                detailed_user_stats.mastered_percentage = mastered_count / exercises_count * 100
 
-    exercise_querry = await db.exec(select(Exercise).where(Exercise.id == exerciseLog.exercise_id))
-    exercise = exercise_querry.first()
-    category_id = str(exercise.exercise_type.id)
+        exercise_query = await db.exec(select(Exercise).where(Exercise.id == exercise_log.exercise_id))
+        exercise = exercise_query.one()
+        category_type = str(exercise.exercise_type.type)
 
-    category_scores = dict(detailed_user_stats.averageScoreByCategory or {})
+        category_scores = dict(detailed_user_stats.average_score_by_category or {})
 
-    old_category_stats = category_scores.get(category_id,{"score": 0.0, "count": 0})
-    old_category_score = old_category_stats["score"]
-    old_category_count = old_category_stats["count"]
+        old_category_stats = category_scores.get(category_type,{"score": 0.0, "count": 0})
+        old_category_score = old_category_stats["score"]
+        old_category_count = old_category_stats["count"]
 
-    new_category_count = old_category_count + 1
-    new_category_score = (old_category_count * old_category_score + exerciseLog.time_in_tune) / new_category_count
+        new_category_count = old_category_count + 1
+        new_category_score = (old_category_count * old_category_score + exercise_log.time_in_tune) / new_category_count
 
-    category_scores[category_id] = {"score": new_category_score, "count": new_category_count}
+        category_scores[category_type] = {"score": new_category_score, "count": new_category_count}
 
-    detailed_user_stats.averageScoreByCategory = category_scores
+        detailed_user_stats.average_score_by_category = category_scores
+        detailed_user_stats.total_practice_time += exercise_log.exercise_duration
 
-    # Without this, the column will not be updated
-    flag_modified(detailed_user_stats, "averageScoreByCategory")
+        # Without this, the column will not be updated
+        flag_modified(detailed_user_stats, "average_score_by_category")
 
-    await actualize_user_streak(detailed_user_stats, db)
+        await actualize_user_streak(detailed_user_stats, db)
 
-    await db.commit()
-    await db.refresh(detailed_user_stats)
+        await db.commit()
+        await db.refresh(detailed_user_stats)
 
 
